209f7968cfaa98c49d8d713637b868c8ac7a6973
SammCro <iso.oztrk@outlook.com>
2023-02-02 15:13:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard></Dashboard>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from 'semantic-ui-react'
+
+function Dashboard() {
+  return (
+    <div>
+        <Button>
+            
+        </Button>
+    </div>
+  )
+}
+
+export default Dashboard;
