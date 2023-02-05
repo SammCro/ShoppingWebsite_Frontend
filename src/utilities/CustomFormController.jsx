ec884eb047f95bd2ad859fc01f50b9f8b728a68a
SammCro <iso.oztrk@outlook.com>
2023-02-05 16:24:15 +0300

    comments are added

--- src/utilities/CustomFormController.jsx
+++ src/utilities/CustomFormController.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 import {  useField, } from "formik";
 import { FormField, Label } from "semantic-ui-react";
 
-
+//Custom form ve diğer componentler oluşturmak için kullanılıyor, kullanıldığı yerden aldığı propslarla işlem yapıyor.
 function CustomFormController(props) {
     const[field,meta] = useField(props);
-
   return (
     <FormField error={meta.touched && !!meta.error}>
       <input {...field} {...props}></input>
