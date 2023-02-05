b4798d61cb8d0be070356315ae63eb0d8caef640
SammCro <iso.oztrk@outlook.com>
2023-02-05 12:16:14 +0300

    form is updated

--- src/utilities/CustomFormController.jsx
+++ src/utilities/CustomFormController.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 
-import {  Field, ErrorMessage } from "formik";
+import {  useField, } from "formik";
 import { FormField, Label } from "semantic-ui-react";
 
 
 function CustomFormController(props) {
-    const{name,placeholder} = props;
+    const[field,meta] = useField(props);
+
   return (
-    <FormField>
-            <Field name={name} placeholder={placeholder}></Field>
-            <ErrorMessage
-              name={name}
-              render={(error) => (
-                <Label pointing basic content={error} color="red"></Label>
-              )}
-            ></ErrorMessage>
+    <FormField error={meta.touched && !!meta.error}>
+      <input {...field} {...props}></input>
+      {meta.touched && !!meta.error  ? (
+        <Label pointing basic color='red' content={meta.error}></Label>
+      ) : null}
     </FormField>
   )
 }
